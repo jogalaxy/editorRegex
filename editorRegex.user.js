d957fff39013d089bd547c36955d734a7a0897ac
jogalaxy <jo123456789@gmx.fr>
2015-04-21 17:32:16 +0200

    compabilité avec firefox

--- editorRegex.user.js
+++ editorRegex.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         LeekWars Editeur Regex
-// @version      0.4
+// @version      0.5
 // @description  Ajout de la prise en charge des regex dans l'éditeur
 // @author       jojo123
 // @downloadURL  https://raw.githubusercontent.com/jogalaxy/editorRegex/master/editorRegex.user.js
@@ -9,175 +9,179 @@
 // @grant        none
 // ==/UserScript==
 
-function regex_open(content)
-{
-	var previousContent = "";
-	while(content != previousContent)
+window.addEventListener('load', function() {
+
+	function regex_open(content)
 	{
-		previousContent = content;
-		content = content.replace(/([a-zA-Z\])]+)\[('|")([a-zA-Z]+)('|")\]/g, '$1.$3');
+		var previousContent = "";
+		while(content != previousContent)
+		{
+			previousContent = content;
+			content = content.replace(/([a-zA-Z\])]+)\[('|")([a-zA-Z]+)('|")\]/g, '$1.$3');
+		}
+
+		previousContent = "";
+		while(content != previousContent)
+		{
+			previousContent = content;
+			content = content.replace(/function ([a-zA-Z0-9]+)\(\)[\r\n]+{[\r\n\t]+var this = \[\];[\r\n]+([^]+)[\r\n\t]+return @this;[\r\n]+}/g, "class $1\r\n{\r\n$2}");
+		}
+		return content;
 	}
 
-	previousContent = "";
-	while(content != previousContent)
+	function regex_save(content)
 	{
-		previousContent = content;
-		content = content.replace(/function ([a-zA-Z0-9]+)\(\)[\r\n]+{[\r\n\t]+var this = \[\];[\r\n]+([^]+)[\r\n\t]+return @this;[\r\n]+}/g, "class $1\r\n{\r\n$2}");
-	}
-	return content;
-}
+		var previousContent = "";
+		while(content != previousContent)
+		{
+			previousContent = content;
+			content = content.replace(/class ([a-zA-Z0-9]+)[\r\n\s]+?{([^]+)}/g, "function $1()\r\n{\r\n\tvar this = [];\r\n$2\treturn @this;\r\n}");
+		}
 
-function regex_save(content)
-{
-	var previousContent = "";
-	while(content != previousContent)
-	{
-		previousContent = content;
-		content = content.replace(/class ([a-zA-Z0-9]+)[\r\n\s]+?{([^]+)}/g, "function $1()\r\n{\r\n\tvar this = [];\r\n$2\treturn @this;\r\n}");
-	}
+		previousContent = "";
+		while(content != previousContent)
+		{
+			previousContent = content;
+			content = content.replace(/([a-zA-Z\])]+)\.([a-zA-Z]+)/g, "$1['$2']");
+		}
 
-	previousContent = "";
-	while(content != previousContent)
-	{
-		previousContent = content;
-		content = content.replace(/([a-zA-Z\])]+)\.([a-zA-Z]+)/g, "$1['$2']");
+		return content;
 	}
 
-	return content;
-}
+	for (var editor in editors)
+	{
 
-for (var editor in editors)
-{
+		editors[editor].load = function(show) {
 
-	editors[editor].load = function(show) {
-
-		var editor = this;
-		
-		ajax('editor_update', {id: this.id, load: true}, function(data) {
-
-			data = regex_open(data);
-
-			// Là y'a un souci, le code présente une ligne de plus :/ On la dégage
-			data = data.slice(0, -1);
+			var editor = this;
 			
-			if (_BASIC) {
+			ajax('editor_update', {id: this.id, load: true}, function(data) {
+
+				data = regex_open(data);
+
+				// Là y'a un souci, le code présente une ligne de plus :/ On la dégage
+				data = data.slice(0, -1);
 				
-				editor.editorDiv.append("<textarea>" + data + "</textarea>");
-				_editorResize();
+				if (_BASIC) {
+					
+					editor.editorDiv.append("<textarea>" + data + "</textarea>");
+					_editorResize();
 
-			} else {
-				editor.editor.setValue(data);
-				editor.editor.getDoc().clearHistory();
+				} else {
+					editor.editor.setValue(data);
+					editor.editor.getDoc().clearHistory();
 
-				setTimeout(function() {
-					editor.updateIncludes();
-				}, 200);
-		 	}
-			
-			editor.loaded = true;
-			if (show) {
-				editor.show();
-			}
-		});
-	}
-
-	editors[editor].save = function() {
-
-		if (_saving) return;
-		_saving = true;
-		
-		var editor = this;
-		
-		_log("save id " + this.id + "...");
-		
-		this.tabDiv.removeClass("modified");
-		
-		$('#compiling').show();
-		$('#results').empty().hide();
-		
-		var saveID = this.id > 0 ? this.id : 0;
-		
-		var content = _BASIC ? this.editorDiv.find('textarea').val() : this.editor.getValue();
-		content = regex_save(content);
-
-		ajax('editor_update', {id: saveID, compile: true, code: content}, function(data) {
-
-			_saving = false;
-			$('#results').empty().show();
-			$('#compiling').hide();
-
-			var error = false;
-			try {
-				data = JSON.parse(data);
-			} catch (e) {
-				error = true;
-			}
-
-			if (data.length == 0 || error) {
+					setTimeout(function() {
+						editor.updateIncludes();
+					}, 200);
+			 	}
 				
-				$('#results').append("<div class='error'>× <i>" + _lang('editor_server_error') + "</i></div>");
-				return;
-			}
-
-			for (var r in data) {
-
-				var res = data[r];
-				var code = res[0];
-				var ia = res[1];
-
-				var iaEditor = editors[ia];
-				var iaName = iaEditor.name;
-
-				if (code == 2) {
-					
-					$('#results').append("<div class='good'>✓ " + _lang('editor_valid_ai', iaName) + "</div><br>");
-					$('#results .good').last().delay(800).fadeOut(function() {
-						$('#results').hide();
-					});
-
-					iaEditor.error = false;
-					iaEditor.tabDiv.removeClass("error");
-					$('.line-error').removeClass("line-error");
-
-					iaEditor.level = res[3];
-					if (ia == current) {
-						$('#comp-level').text(res[3]);
-					}
-					
-				} else if (code == 1) {
-
-					var info = res[2];
-					
-					$('#results').append("<div class='error'>× <b>" + iaName + "</b>&nbsp; ▶ " + info + "</div><br>");
-					iaEditor.tabDiv.removeClass("error").addClass("error");
-					iaEditor.error = true;
-
-				} else if (code == 0) {
-
-					var line = res[3];
-					var pos = res[4];
-					var info = res[5];
-					
-					$('#results').append("<div class='error'>× " + _lang('editor_ai_error', iaName, line) + "&nbsp; ▶ " + info + "</div><br>");
-					
-					iaEditor.tabDiv.removeClass("error").addClass("error");
-					
-					iaEditor.error = true;
-					iaEditor.errorLine = line;
-					
-					iaEditor.showErrors();
+				editor.loaded = true;
+				if (show) {
+					editor.show();
 				}
-			}
+			});
+		}
+
+		editors[editor].save = function() {
+
+			if (_saving) return;
+			_saving = true;
 			
-			editor.modified = false;
+			var editor = this;
 			
-			if (editor.needTest) {
-				editor.needTest = false;
-				editor.test();
-			}
-		});
+			_log("save id " + this.id + "...");
+			
+			this.tabDiv.removeClass("modified");
+			
+			$('#compiling').show();
+			$('#results').empty().hide();
+			
+			var saveID = this.id > 0 ? this.id : 0;
+			
+			var content = _BASIC ? this.editorDiv.find('textarea').val() : this.editor.getValue();
+			content = regex_save(content);
+
+			ajax('editor_update', {id: saveID, compile: true, code: content}, function(data) {
+
+				_saving = false;
+				$('#results').empty().show();
+				$('#compiling').hide();
+
+				var error = false;
+				try {
+					data = JSON.parse(data);
+				} catch (e) {
+					error = true;
+				}
+
+				if (data.length == 0 || error) {
+					
+					$('#results').append("<div class='error'>× <i>" + _lang('editor_server_error') + "</i></div>");
+					return;
+				}
+
+				for (var r in data) {
+
+					var res = data[r];
+					var code = res[0];
+					var ia = res[1];
+
+					var iaEditor = editors[ia];
+					var iaName = iaEditor.name;
+
+					if (code == 2) {
+						
+						$('#results').append("<div class='good'>✓ " + _lang('editor_valid_ai', iaName) + "</div><br>");
+						$('#results .good').last().delay(800).fadeOut(function() {
+							$('#results').hide();
+						});
+
+						iaEditor.error = false;
+						iaEditor.tabDiv.removeClass("error");
+						$('.line-error').removeClass("line-error");
+
+						iaEditor.level = res[3];
+						if (ia == current) {
+							$('#comp-level').text(res[3]);
+						}
+						
+					} else if (code == 1) {
+
+						var info = res[2];
+						
+						$('#results').append("<div class='error'>× <b>" + iaName + "</b>&nbsp; ▶ " + info + "</div><br>");
+						iaEditor.tabDiv.removeClass("error").addClass("error");
+						iaEditor.error = true;
+
+					} else if (code == 0) {
+
+						var line = res[3];
+						var pos = res[4];
+						var info = res[5];
+						
+						$('#results').append("<div class='error'>× " + _lang('editor_ai_error', iaName, line) + "&nbsp; ▶ " + info + "</div><br>");
+						
+						iaEditor.tabDiv.removeClass("error").addClass("error");
+						
+						iaEditor.error = true;
+						iaEditor.errorLine = line;
+						
+						iaEditor.showErrors();
+					}
+				}
+				
+				editor.modified = false;
+				
+				if (editor.needTest) {
+					editor.needTest = false;
+					editor.test();
+				}
+			});
+		}
+
 	}
 
-	if (editors[editor].loaded) editors[editor].load();
+	editors[current].load();
 
-}
+}, false);
